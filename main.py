2bf6fb421fdfdbb215b4902753ea519942884182
kumar-harshh <kh425718@gmail.com>
2024-01-22 01:20:30 +0530

    Final Commit

--- main.py
+++ main.py
@@ -1,109 +1,90 @@
 import streamlit as st
+import PyPDF2 as pdf
+from langchain.text_splitter import CharacterTextSplitter as ct
+from langchain.embeddings.cohere import CohereEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from langchain_community.llms import Cohere
+from chatbot_style import css, bot_template, user_template
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key=os.getenv("COHERE_API_KEY")
+
+#To persist the conversation context
+def get_conversation_chain(vector_store):
+    llm=Cohere(model="command")
+    memory=ConversationBufferMemory(memory_key="chat_history", return_messages=True, human_prefix="Human", ai_prefix="Bot")
+    conversation_chain=ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vector_store.as_retriever(),
+        memory=memory,
+    )
+    return conversation_chain
+    
 
 st.title("Chat with multiple PDFs 👋🏻")
-st.text_input("Hi! How May I Help?")
+st.write("You Must Ensure that you have uploaded the PDF files before asking a question")
+question=st.text_input("Hi! How May I Help?")
+
+#Initialising Sesson States
+if "conversation" not in st.session_state:
+    st.session_state.conversation=None
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history=None
+    
+#Process Flow-> Upload PDF-> Extract Text -> Chunk them up-> Generate Embeddings->
+# -> Store them in the Vector DB -> Chat Chain
+
+st.write(css, unsafe_allow_html=True)
+
+if question:
+    response=st.session_state.conversation({"question":question})
+    st.session_state.chat_history=response["chat_history"]
+    for i, message in enumerate(st.session_state.chat_history):
+        if i%2==0:
+            st.write(user_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
+
+                    
+    
 with st.sidebar:
     st.header("Upload your PDFs 📚")
-    st.file_uploader("Select your PDF files & Click on 'Process' to proceed")
-    st.button("Process")
-
-
-
-
-
-
-
-
-
-
-#Streamlit Basic tutorial
-
-
-# import pandas as pd
-# import time
-
-# # Display text, data, and objects.
-# st.write("Hello, Streamlit!")
-# st.write() 
-
-# #Display titles and headers.
-# st.title("My Streamlit App")
-# st.header("Chat with Multiple PDF")
-# st.subheader("Now you can chat with your complex documents, Snap") 
-
-# # Display plain text.
-# st.text("Lorem Ipsum")
-
-# #Displaying Data:
-# #DataFrame
-
-# data={
-#     'Name':['Harsh', 'Tua', 'Ashish', 'Anmol'],
-#     'Age':[23,22,21,31],
-#     'City':['BLR', 'KOAA', 'KOAA', 'BLR']
-# }
-
-# df=pd.DataFrame(data)
-# st.dataframe(df)# Display a DataFrame.
-
-# #Table Data
-
-# table_data=[
-#     ['Harsh', 23, 'BLR'],
-#     ['Tua', 22, 'KOAA']
-# ]
-# st.table(table_data)# Display a static table.
-
-# #JSON Data
-
-# json_data={
-#     "Name":"Harsh",
-#     "Age":23,
-#     "City":"BLR",
-#     "Hobbies":["Cooking", "Cricket", "Reading Books"]
-# }
-
-# st.json(json_data)# Display JSON data.
-
-# #Interactive Widgets:
-
-# #: Create interactive widgets.
-# st.button("Label: Click Here")
-# st.checkbox("Label: Checklist1")
-# st.checkbox("Label: Checklist2")
-
-# st.radio("Radio", ["Tag1", "Tag2", "Tag3"])
-# st.selectbox("Dropdown", ["Option1", "Option2", "Option3"])
-
-# # Create sliders and selectors.
-# st.slider("Slider", 0,10)
-# st.select_slider("Select Slider", ["Option1", "Option2", "Option3"])
-# st.multiselect("Multi Select", ["Option1", "Option2", "Option3"])
-
-# #: Accept user inputs
-# st.text_input("Text Input: Enter Text")
-# st.number_input("Number Input: Enter a Number")
-# st.text_area("Text Area")
-
-# #Adding Sidebat
-# add_selectbox = st.sidebar.selectbox(
-#     'How would you like to be contacted?',
-#     ('Email', 'Home phone', 'Mobile phone')
-# )
-
-# # Add a slider to the sidebar:
-# add_slider = st.sidebar.slider(
-#     'Select a range of values',
-#     0.0, 100.0, (25.0, 75.0)
-# )
-
-# #Progress Bar
-# latest_iteration = st.empty()
-# bar = st.progress(0)
-
-# for i in range(100):
-#   # Update the progress bar with each iteration.
-#   latest_iteration.text(f'Iteration {i+1}')
-#   bar.progress(i + 1)
-#   time.sleep(0.1)
+    doc_pdf_files=st.file_uploader("Select your PDF files & Click on 'Process' to proceed", accept_multiple_files=True)
+    submit=st.button("Process")
+    if submit:
+        with st.spinner("Processing! Wait a While...."):
+            #Extract PDF text-Using PyPDF2 pdfreader method
+            text = ""
+            for file in doc_pdf_files:
+                reader = pdf.PdfReader(file)
+                for page_number in range(len(reader.pages)):
+                    page = reader.pages[page_number]
+                    text += str(page.extract_text())
+            
+            
+            #Chunk them up- Text Chunking using langchain textsplitter
+            text_chunker= ct(
+                separator="\n",
+                chunk_size=1000,
+                chunk_overlap=200,
+                length_function=len,
+                add_start_index = True
+            )
+            text_chunk=text_chunker.split_text(text)
+                        
+            #Create Embeddings & Store them in the Vector Database
+            embeddings = CohereEmbeddings(model="embed-english-light-v3.0")
+            vector_store=FAISS.from_texts(texts=text_chunk, embedding=embeddings)
+            
+            if embeddings:
+                st.write("✅ File is Uploaded & Embeded, You may ask your questions now")
+            else:
+                st.write("❗️ Failed to process the Document")
+                
+            #Create Chat Buffer Memory using langchain
+            st.session_state.conversation=get_conversation_chain(vector_store)
 
